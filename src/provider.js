8393821f487dbeccd50709be2c38300d395f483c
Harry Wolff <hswolff@gmail.com>
2016-02-25 08:30:30 -0500

    Add redux-storage to save state in AsyncStorage

--- src/provider.js
+++ src/provider.js
@@ -2,8 +2,14 @@ import React, {
   Component,
   PropTypes,
 } from 'react-native';
-import * as store from './store';
+import {
+  createMiddleware,
+  createStore,
+} from './store';
 import { Provider } from 'react-redux';
+import * as storage from 'redux-storage';
+import createEngine from 'redux-storage-engine-reactnativeasyncstorage';
+import reduxStorageDebounce from 'redux-storage-decorator-debounce';
 
 export default class HNProvider extends Component {
   static propTypes = {
@@ -14,8 +20,14 @@ export default class HNProvider extends Component {
   };
 
   componentWillMount() {
-    this.setState({
-      store: store.create(),
+    const engine = reduxStorageDebounce(createEngine('reduxStore'), 1500);
+
+    engine.load().then(storedState => {
+      const middleware = createMiddleware(storage.createMiddleware(engine));
+
+      this.setState({
+        store: createStore(storedState, middleware),
+      });
     });
   }
 
